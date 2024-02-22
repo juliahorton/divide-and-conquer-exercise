0adc06021700a65498895b80214bdfc2edf94522
Julia Horton <juliahorton00@gmail.com>
2024-02-22 04:17:53 -0600

    count-zeroes and find-rotated-index completed

--- count-zeroes.js
+++ count-zeroes.js
@@ -2,13 +2,32 @@
 
 // Constraints: time complexity O(log n)
 
-// countZeroes([1,1,1,1,0,0]) // 2
+
+//              L       M         R
+//                        L   M   R
+//                      
+// countZeroes([1,1,1,1,1,1,0,0,0,0]) // 4
 // countZeroes([1,0,0,0,0]) // 4
 // countZeroes([0,0,0]) // 3
 // countZeroes([1,1,1,1]) // 0
 
-function countZeroes() {
-    
+function countZeroes(arr) {
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
+
+    while (leftIdx <= rightIdx){
+        let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+        let middleVal = arr[middleIdx];
+
+        if (middleVal !== 0){
+            leftIdx = middleIdx + 1
+        } else if (middleVal === 0 && arr[leftIdx] !== 0){
+            rightIdx = middleIdx
+        } else {
+            return arr.slice(leftIdx).length
+        }
+    }
+    return 0
 }
 
 module.exports = countZeroes
--- find-rotated-index.js
+++ find-rotated-index.js
@@ -8,8 +8,55 @@
 // findRotatedIndex([37,44,66,102,10,22],14) // -1
 // findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 12) // -1
 
-function findRotatedIndex() {
- 
+function findRotatedIndex(arr, num){
+    const pivot = findPivotPoint(arr);
+    if (pivot > 0 && num <= arr[pivot - 1] && num >= arr[0]){
+        return binarySearch(arr, num, 0, pivot - 1)
+    } else {
+        return binarySearch(arr, num, pivot, arr.length - 1)
+    }
 }
+
+function binarySearch(arr, target, leftIdx = 0, rightIdx = arr.length - 1){
+    if (arr.length === 0) return -1;
+    if (target < arr[leftIdx] || target > arr[rightIdx]) return -1;
+
+    while (leftIdx <= rightIdx){
+        let middleIdx = Math.floor((rightIdx + leftIdx) / 2);
+        let middleVal = arr[middleIdx];
+
+        if (middleVal < target){
+            leftIdx = middleIdx + 1
+        }
+
+        else if (middleVal > target){
+            rightIdx = middleIdx -1
+        }
+        else {
+            return middleIdx
+        }
+    }
+    return -1
+}
+
+function findPivotPoint(arr){
+    if (arr.length === 1 || arr[0] < arr[arr.length - 1]) return 0;
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
+
+    while (leftIdx <= rightIdx) {
+        let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+        let middleVal = arr[middleIdx];
+
+        if (middleVal < arr[middleIdx - 1]){
+            return middleIdx
+        } else if (arr[leftIdx] >= arr[rightIdx]){
+            leftIdx = middleIdx + 1;
+        } else {
+            rightIdx = middleIdx - 1;
+        }
+    }
+}
+
 
 module.exports = findRotatedIndex
