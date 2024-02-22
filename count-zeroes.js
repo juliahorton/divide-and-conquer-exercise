ad272f439100387f96d75c2809bc40ae6c466943
Julia Horton <juliahorton00@gmail.com>
2024-02-21 23:05:04 -0600

    instructions for each practice problem added to corresponding files

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,5 +1,14 @@
+// Given an array of 1s and 0s which has all 1s first followed by all 0s, write a function called countZeroes, which returns the number of zeroes in the array.
+
+// Constraints: time complexity O(log n)
+
+// countZeroes([1,1,1,1,0,0]) // 2
+// countZeroes([1,0,0,0,0]) // 4
+// countZeroes([0,0,0]) // 3
+// countZeroes([1,1,1,1]) // 0
+
 function countZeroes() {
-  
+    
 }
 
 module.exports = countZeroes
--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,3 +1,12 @@
+// Given a sorted array and a number, write a function called sortedFrequency that counts the occurrences of the number in the array
+
+// Constraints: time complexity O(log n)
+
+// sortedFrequency([1,1,2,2,2,2,3],2) // 4
+// sortedFrequency([1,1,2,2,2,2,3],3) // 1
+// sortedFrequency([1,1,2,2,2,2,3],1) // 2
+// sortedFrequency([1,1,2,2,2,2,3],4) // -1
+
 function sortedFrequency() {
 
 }
