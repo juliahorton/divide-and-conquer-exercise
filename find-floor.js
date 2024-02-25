0881281ee7d2d9f66ef9bbe1609a7995e2526107
Julia Horton <juliahorton00@gmail.com>
2024-02-24 19:50:47 -0600

    completed find-floor

--- find-floor.js
+++ find-floor.js
@@ -6,8 +6,32 @@
 // findFloor([1,2,8,10,10,12,19], 20) // 19
 // findFloor([1,2,8,10,10,12,19], 0) // -1
 
-function findFloor() {
-  
+function findFloor(arr, num) {
+
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
+
+
+    if (arr[0] > num) return -1;
+    if (arr.length === 1 && arr[0] <= num) return arr[0];
+    if (arr[rightIdx] <= num) return arr[rightIdx];
+
+    while (leftIdx <= rightIdx){
+        let middleIdx = Math.floor((rightIdx + leftIdx) / 2);
+        let middleVal = arr[middleIdx];
+
+        if (middleVal <= num && arr[middleIdx + 1] > num){
+            return middleVal
+        }
+
+        else if (middleVal < num){
+            leftIdx = middleIdx;
+        }
+
+         else if (middleVal > num){
+            rightIdx = middleIdx
+        }
+    }
 }
 
 module.exports = findFloor
